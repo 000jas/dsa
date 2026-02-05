103e9895c595e1f9abc4c9fa6b2d4e84e0787a1b
Jheel Pashine <jppashine@gcoen.ac.in>
2026-02-05 23:50:48 +0530

    Push and pop on stack

---
+++ push-pop.sh
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#define MAX 10
+
+int stack[MAX];
+int top = -1;
+
+// Function to push element
+void push(int item) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        stack[++top] = item;
+        printf("Element pushed\n");
+    }
+}
+
+// Function to pop element
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("Popped element: %d\n", stack[top--]);
+    }
+}
+
+// Function to display stack
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements:\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main() {
+    int choice, item;
+
+    while (1) {
+        printf("\n1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter element to push: ");
+                scanf("%d", &item);
+                push(item);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
